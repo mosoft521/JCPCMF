e02d8d23e33ab1120ca6eb71e7bad1f051c50cf6
Alvin Zhang <mosoft521@gmail.com>
2016-12-02 12:54:34 +0800

    第一个程序

---
+++ src/main/java/com/gmail/mosoft521/jcpcmf/ch01Semaphore/ch01SemaphoreTest1/extthread/ThreadC.java
@@ -0,0 +1,19 @@
+package com.gmail.mosoft521.jcpcmf.ch01Semaphore.ch01SemaphoreTest1.extthread;
+
+import com.gmail.mosoft521.jcpcmf.ch01Semaphore.ch01SemaphoreTest1.service.Service;
+
+public class ThreadC extends Thread {
+
+	private Service service;
+
+	public ThreadC(Service service) {
+		super();
+		this.service = service;
+	}
+
+	@Override
+	public void run() {
+		service.testMethod();
+	}
+
+}
---
+++ src/main/java/com/gmail/mosoft521/jcpcmf/ch01Semaphore/ch01SemaphoreTest1/test/Run.java
@@ -0,0 +1,30 @@
+package com.gmail.mosoft521.jcpcmf.ch01Semaphore.ch01SemaphoreTest1.test;
+
+import com.gmail.mosoft521.jcpcmf.ch01Semaphore.ch01SemaphoreTest1.extthread.ThreadA;
+import com.gmail.mosoft521.jcpcmf.ch01Semaphore.ch01SemaphoreTest1.extthread.ThreadB;
+import com.gmail.mosoft521.jcpcmf.ch01Semaphore.ch01SemaphoreTest1.extthread.ThreadC;
+import com.gmail.mosoft521.jcpcmf.ch01Semaphore.ch01SemaphoreTest1.service.Service;
+
+public class Run {
+
+    public static void main(String[] args) {
+        Service service = new Service();
+        ThreadA a = new ThreadA(service);
+        a.setName("A");
+        ThreadB b = new ThreadB(service);
+        b.setName("B");
+        ThreadC c = new ThreadC(service);
+        c.setName("C");
+        a.start();
+        b.start();
+        c.start();
+    }
+}
+/*
+A begin timer=1480654366966
+A   end timer=1480654371967
+C begin timer=1480654371967
+C   end timer=1480654376967
+B begin timer=1480654376967
+B   end timer=1480654381967
+ */
