91e6d5b3bc25fb8ba96d42247d71a89fe8fac204
Alvin Zhang <mosoft521@gmail.com>
2016-12-04 17:37:10 +0800

    (\r\n)*2->1

--- src/main/java/com/gmail/mosoft521/jcpcmf/ch01Semaphore/p001SemaphoreTest1/service/Service.java
+++ src/main/java/com/gmail/mosoft521/jcpcmf/ch01Semaphore/p001SemaphoreTest1/service/Service.java
@@ -8,11 +8,9 @@ public class Service {
     public void testMethod() {
         try {
             semaphore.acquire();
-            System.out.println(Thread.currentThread().getName()
-                    + " begin timer=" + System.currentTimeMillis());
+            System.out.println(Thread.currentThread().getName() + " begin timer=" + System.currentTimeMillis());
             Thread.sleep(5000);
-            System.out.println(Thread.currentThread().getName()
-                    + "   end timer=" + System.currentTimeMillis());
+            System.out.println(Thread.currentThread().getName() + "   end timer=" + System.currentTimeMillis());
             semaphore.release();
         } catch (InterruptedException e) {
             e.printStackTrace();
