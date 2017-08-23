5d3493a08049c0771961eb6619f3bf556658d567
zhangjiawen <zhangjiawen@vcredit.com>
2017-08-23 09:03:33 +0800

    添加了一个todo

--- src/main/java/com/gmail/mosoft521/jcpcmf/ch01Semaphore/p003Semaphore_acquire/service/Service.java
+++ src/main/java/com/gmail/mosoft521/jcpcmf/ch01Semaphore/p003Semaphore_acquire/service/Service.java
@@ -8,14 +8,11 @@ public class Service {
     public void testMethod() {
         try {
             semaphore.acquire(2);
-            System.out.println(Thread.currentThread().getName()
-                    + " begin timer=" + System.currentTimeMillis());
+            System.out.println(Thread.currentThread().getName() + " begin timer=" + System.currentTimeMillis());
             int sleepValue = ((int) (Math.random() * 10000));
-            System.out.println(Thread.currentThread().getName() + " 停止了"
-                    + (sleepValue / 1000) + "秒");
+            System.out.println(Thread.currentThread().getName() + " 停止了" + (sleepValue / 1000) + "秒");
             Thread.sleep(sleepValue);
-            System.out.println(Thread.currentThread().getName()
-                    + "   end timer=" + System.currentTimeMillis());
+            System.out.println(Thread.currentThread().getName() + "   end timer=" + System.currentTimeMillis());
             semaphore.release(2);
         } catch (InterruptedException e) {
             e.printStackTrace();
--- src/main/java/com/gmail/mosoft521/jcpcmf/ch01Semaphore/p003Semaphore_acquire/test/Run.java
+++ src/main/java/com/gmail/mosoft521/jcpcmf/ch01Semaphore/p003Semaphore_acquire/test/Run.java
@@ -14,6 +14,7 @@ public class Run {
     }
 }
 /*
+todo:这个列子演示了Semaphore其实并发并不安全,可以使用jcstress来测试下
 Thread-2 begin timer=1480666799883
 Thread-3 begin timer=1480666799883
 Thread-1 begin timer=1480666799883
@@ -44,4 +45,35 @@ Thread-9   end timer=1480666803996
 Thread-1   end timer=1480666804652
 Thread-4   end timer=1480666806116
 Thread-2   end timer=1480666808944
+-----------------------------
+Thread-1 begin timer=1503449903109
+Thread-2 begin timer=1503449903109
+Thread-0 begin timer=1503449903109
+Thread-2 停止了2秒
+Thread-0 停止了0秒
+Thread-1 停止了0秒
+Thread-4 begin timer=1503449903110
+Thread-4 停止了2秒
+Thread-5 begin timer=1503449903110
+Thread-5 停止了8秒
+Thread-0   end timer=1503449903244
+Thread-6 begin timer=1503449903244
+Thread-6 停止了8秒
+Thread-1   end timer=1503449904055
+Thread-3 begin timer=1503449904055
+Thread-3 停止了1秒
+Thread-3   end timer=1503449905315
+Thread-7 begin timer=1503449905315
+Thread-7 停止了9秒
+Thread-2   end timer=1503449905806
+Thread-9 begin timer=1503449905806
+Thread-9 停止了7秒
+Thread-4   end timer=1503449906015
+Thread-8 begin timer=1503449906015
+Thread-8 停止了2秒
+Thread-8   end timer=1503449908279
+Thread-5   end timer=1503449912063
+Thread-6   end timer=1503449912090
+Thread-9   end timer=1503449912815
+Thread-7   end timer=1503449915102
  */
